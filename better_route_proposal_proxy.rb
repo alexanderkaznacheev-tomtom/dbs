980e2ea2520a55846dfebc6c741f1962fe3c12f9
Alexander Kaznacheev <alexander.kaznacheev@tomtom.com>
2025-06-26 00:29:43 +0200

    add 5 modes

--- better_route_proposal_proxy.rb
+++ better_route_proposal_proxy.rb
@@ -11,8 +11,14 @@ PORT = 8000
 TARGET_BASE = "https://api.tomtom.com"
 
 # Global variable for additional processing mode.
-# Possible values: :enable1, :enable2, :disable
-$processing_mode = :enable1
+# Possible values: :mode1, :mode2, :mode3, :mode4, :mode5
+# Modes meaning:
+#  • mode1: Adds the first alternative (index 1) and sets a delay of 1 hour (3600 sec)
+#  • mode2: Adds the first alternative (index 1) and sets a delay of 3 minutes (180 sec)
+#  • mode3: Adds the second alternative (index 2) and sets a delay of 1.5 hours (5400 sec)
+#  • mode4: Adds the second alternative (index 2) and sets a delay of 2 minutes (120 sec)
+#  • mode5: Returns the response unmodified
+$processing_mode = :mode1
 
 def clean_url(original_url)
   uri = URI.parse(original_url)
@@ -55,15 +61,15 @@ end
 server = WEBrick::HTTPServer.new(Port: PORT)
 
 server.mount_proc '/' do |req, res|
-  # Construct the URL for the target request
+  # Construct the URL for the outgoing request
   target_url = "#{TARGET_BASE}#{req.unparsed_uri}"
   target_uri = URI(target_url)
 
-  # Set up HTTPS connection to the target server
+  # Set up HTTPS connection to the destination server
   http = Net::HTTP.new(target_uri.host, target_uri.port)
   http.use_ssl = (target_uri.scheme == "https")
 
-  # Select the request class (GET, POST, PUT, DELETE, etc.)
+  # Select the HTTP class (GET, POST, PUT, DELETE, etc.) based on the request method
   http_class = Net::HTTP.const_get(req.request_method.capitalize)
   request_forward = http_class.new(target_uri.request_uri)
 
@@ -79,15 +85,12 @@ server.mount_proc '/' do |req, res|
   # Transfer request body (if present)
   request_forward.body = req.body if req.body
 
-  # Perform the request to the target server
+  # Perform the request to the destination server
   response_forward = http.request(request_forward)
 
-  extra_response = nil
-
-  # If additional processing is enabled (mode is not :disable),
-  # and this is a POST request, and the URL contains "calculateRoute",
-  # then perform an additional GET request using the cleaned URL.
-  if $processing_mode != :disable && req.request_method == "POST" && req.unparsed_uri.include?("calculateRoute")
+  # If the mode is not mode5, the request is POST, and the URL contains "calculateRoute",
+  # perform an additional GET request and modify the JSON response.
+  if $processing_mode != :mode5 && req.request_method == "POST" && req.unparsed_uri.include?("calculateRoute")
     cleaned_url = clean_url(target_url)
     extra_uri = URI.parse(cleaned_url)
     extra_http = Net::HTTP.new(extra_uri.host, extra_uri.port)
@@ -107,13 +110,15 @@ server.mount_proc '/' do |req, res|
     end
 
     begin
-      # Parse JSON responses
       json_post = JSON.parse(ungzed_response_body(response_forward))
       json_get  = extra_response ? JSON.parse(ungzed_response_body(extra_response)) : nil
 
-      # Set delay based on processing mode:
-      # In enable1 mode, delay is 3600 seconds; in enable2 mode, delay is 5400 seconds.
-      delay = ($processing_mode == :enable2) ? 5400 : 3600
+      delay = case $processing_mode
+              when :mode1 then 3600   # 1 hour
+              when :mode2 then 180    # 3 minutes
+              when :mode3 then 5400   # 1.5 hours
+              when :mode4 then 120    # 2 minutes
+              end
 
       if json_post["routes"] && json_post["routes"].any?
         first_route = json_post["routes"][0]
@@ -131,7 +136,6 @@ server.mount_proc '/' do |req, res|
           end
         end
 
-        # Delay routes for each leg (if available)
         if first_route["legs"]
           first_route["legs"].each do |leg|
             if leg["summary"]
@@ -151,34 +155,37 @@ server.mount_proc '/' do |req, res|
         end
       end
 
-      # Add an additional route from the extra GET response.
-      # For mode :enable1 choose the second route (index 1),
-      # for mode :enable2 choose the third route (index 2), if available.
+      # Depending on the mode, choose the alternative route from the extra GET response.
       if json_get && json_get["routes"]
-        if $processing_mode == :enable1 && json_get["routes"].size > 1
-          extra_route = json_get["routes"][1]
-          if extra_route["summary"]
-            extra_route["summary"]["planningReason"] = "Better_Proposal"
+        case $processing_mode
+        when :mode1, :mode2
+          # Add the first alternative – index 1 (if available)
+          if json_get["routes"].size > 1
+            extra_route = json_get["routes"][1]
+            if extra_route["summary"]
+              extra_route["summary"]["planningReason"] = "Better_Proposal"
+            end
+            json_post["routes"] << extra_route
           end
-          json_post["routes"] << extra_route
-        elsif $processing_mode == :enable2 && json_get["routes"].size > 2
-          extra_route = json_get["routes"][2]
-          if extra_route["summary"]
-            extra_route["summary"]["planningReason"] = "Better_Proposal"
+        when :mode3, :mode4
+          # Add the second alternative – index 2 (if available)
+          if json_get["routes"].size > 2
+            extra_route = json_get["routes"][2]
+            if extra_route["summary"]
+              extra_route["summary"]["planningReason"] = "Better_Proposal"
+            end
+            json_post["routes"] << extra_route
           end
-          json_post["routes"] << extra_route
         end
       end
 
-      # Update the original response body with the new JSON
       new_body = JSON.dump(json_post)
       response_forward.body = new_body
-      # Remove the encoding header since we return uncompressed content
+      # Remove the encoding header, as we are returning uncompressed content
       response_forward.delete("content-encoding")
       response_forward["content-length"] = new_body.bytesize.to_s
     rescue => e
       puts "JSON processing error: #{e.message}"
-      # If an error occurs during JSON processing, leave the original response unchanged
     end
   end
 
@@ -196,7 +203,12 @@ end
 
 # Thread for processing console commands to switch additional processing modes.
 Thread.new do
-  help_msg = "Commands:\n1 or 'enable1' - Enable additional processing (mode 1)\n2 or 'enable2' - Enable additional processing (mode 2)\n3 or 'disable' - Disable additional processing"
+  help_msg = "Commands:
+1 or 'mode1' - Add the first alternative and set delay to 1 hour
+2 or 'mode2' - Add the first alternative and set delay to 3 minutes
+3 or 'mode3' - Add the second alternative and set delay to 1.5 hours
+4 or 'mode4' - Add the second alternative and set delay to 2 minutes
+5 or 'mode5' - Return the response without modifications"
   puts help_msg
   puts "[Current mode: #{$processing_mode}]"
   loop do
@@ -207,17 +219,23 @@ Thread.new do
       next
     end
     case input
-    when '1', 'enable1'
-      $processing_mode = :enable1
-      puts "[Current mode: enable1]"
-    when '2', 'enable2'
-      $processing_mode = :enable2
-      puts "[Current mode: enable2]"
-    when '3', 'disable'
-      $processing_mode = :disable
-      puts "[Current mode: disabled]"
+    when '1', 'mode1'
+      $processing_mode = :mode1
+      puts "[Current mode: mode1]"
+    when '2', 'mode2'
+      $processing_mode = :mode2
+      puts "[Current mode: mode2]"
+    when '3', 'mode3'
+      $processing_mode = :mode3
+      puts "[Current mode: mode3]"
+    when '4', 'mode4'
+      $processing_mode = :mode4
+      puts "[Current mode: mode4]"
+    when '5', 'mode5'
+      $processing_mode = :mode5
+      puts "[Current mode: mode5]"
     else
-      puts "Unknown command '#{input}'. Valid commands: 'enable1'/'1', 'enable2'/'2' or 'disable'/'3'."
+      puts "Unknown command '#{input}'. Valid commands: 'mode1'/'1', 'mode2'/'2', 'mode3'/'3', 'mode4'/'4', 'mode5'/'5'."
       puts "[Current mode: #{$processing_mode}]"
     end
   end
